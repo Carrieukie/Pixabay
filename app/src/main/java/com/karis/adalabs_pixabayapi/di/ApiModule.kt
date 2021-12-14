ac899080692418062457e833d9b2bbefeff9564d
Carrieukie <kariuki931@gmail.com>
2021-12-14 10:26:30 +0300

    Injected networking depedencies into the dagger graph

--- app/src/main/java/com/karis/adalabs_pixabayapi/di/ApiModule.kt
+++ app/src/main/java/com/karis/adalabs_pixabayapi/di/ApiModule.kt
@@ -1,4 +1,45 @@
 package com.karis.adalabs_pixabayapi.di
 
+import com.karis.adalabs_pixabayapi.data.network.api.ImagesApi
+import com.karis.adalabs_pixabayapi.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class ApiModule {
+
+    @Provides
+    @Singleton
+    fun providesHttpLoggingInterceptor() : HttpLoggingInterceptor{
+       return  HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC);
+    }
+
+    @Provides
+    @Singleton
+    fun providesokHttpClient(loggingInterceptor : HttpLoggingInterceptor ) : OkHttpClient{
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient : OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideImagesApi(retrofit: Retrofit): ImagesApi = retrofit.create(ImagesApi::class.java)
+
 }
