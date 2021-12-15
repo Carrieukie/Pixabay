f34043f5ce15f75154fac28f59a69d8751ed649f
Carrieukie <kariuki931@gmail.com>
2021-12-15 20:49:30 +0300

    root commit

--- app/src/main/java/com/karis/adalabs_pixabayapi/PixabayApiApp.kt
+++
@@ -1,7 +0,0 @@
-package com.karis.adalabs_pixabayapi
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PixabayApiApp : Application()
--- app/src/main/java/com/karis/adalabs_pixabayapi/utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.karis.adalabs_pixabayapi.utils
-
-
-const val BASE_URL = "https://pixabay.com/"
-const val STARTING_PAGE_INDEX = 1
